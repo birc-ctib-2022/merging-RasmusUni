7af1f7b9db891f4939ab2fedac364f7ca146efab
RasmusUni <202004598@post.au.dk>
2022-09-11 11:24:54 +0200

    merging x and y

--- src/merge.py
+++ src/merge.py
@@ -15,8 +15,24 @@ def merge(x: list[int], y: list[int]) -> list[int]:
     z = []  # a new list to copy elements into
     # FIXME: fill out the loop so you merge the lists
     # until one of them is empty
-    while i < len(x) and j < len(y):
-        break  # FIXME: you shouldn't just break here
+    while i <= len(x) and j <= len(y):
+        if x[i] < y[j]:
+            z.append(x[i])
+            i+=1
+        elif y[j] < x[i]:
+            z.append(y[j])
+            j+=1
+        elif x[i] == y[j]:
+            z.append(x[i],y[j])
+            i+=1
+            j+=1
+    
+    if i <= len(x):
+        z+x
+    else:
+        x+y 
+
+    # FIXME: you shouldn't just break here
     # At least one of the lists is empty now. Copy the
     # remainder of the other into z.
     return z
